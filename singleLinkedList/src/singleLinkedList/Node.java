f5a183fcf87058166f475f8710621d2ce9951794
whit3Q <gw1221@naver.com>
2021-07-03 22:38:23 +0900

    20210703

--- singleLinkedList/src/singleLinkedList/Node.java
+++ singleLinkedList/src/singleLinkedList/Node.java
@@ -1,5 +1,98 @@
 package singleLinkedList;
 
 public class Node {
+	static Node end = null;
+	Node next = null;
+	int data;
+	boolean count = false;
+	
+	Node() {
+		this(0);
+	}
+	Node(int a) {
+		data = a;
+	}
+		
+	void append(int b) {
+		Node newNode = new Node(b);
+		if (end == null) { 
+			end = this;
+			Node n = end;
+			n.next = newNode;
+			end = n.next;
+		} else {
+			Node n = end;
+			n.next = newNode;
+			end = n.next;
+			}
+		}
+	void delete(int a) {
+		Node n = this;
+		while(n.next != null) {
+			if(n.next.data == a) {
+				n.next = n.next.next;
+				if(n.next == null) {
+					end = n;
+				}
+			} else {
+				n = n.next;
+			}
+		}
+	}
+	void retvive() {
+		Node n = this;
+		while(n.next != null) {
+			System.out.print(n.data+">");
+			n = n.next;
+		}
+		System.out.println(n.data);
+	}	
 
 }
+	
+	
+//	Node next = null;
+//	int data;
+//	
+//	Node() {
+//		this(0);
+//	}
+//	
+//	Node(int data) {
+//		this.data = data;
+//	}
+//	
+//	boolean append(int a) {
+//		try {
+//			Node end = new Node(a);
+//			Node n = this;
+//			while(n.next != null) {
+//				n = n.next;
+//			}
+//			n.next = end;
+//			return true;
+//		} catch (Exception e1) {
+//			
+//			return false;
+//		}
+//	}
+//	boolean delete(int d) {
+//		Node n = this;
+//		while(n.next !=null) {
+//			if(n.next.data == d) {
+//				n.next = n.next.next;
+//			} else {
+//				n = n.next;
+//			}
+//		}
+//		return true;
+//	}
+//	void retvive() {
+//		Node n = this;
+//		while(n.next != null) {
+//			System.out.print(n.data+">");
+//			n = n.next;
+//		}
+//		System.out.println(n.data);
+//	}
+//}
--- singleLinkedList/src/singleLinkedList/slist.java
+++ singleLinkedList/src/singleLinkedList/slist.java
@@ -1,5 +1,17 @@
 package singleLinkedList;
 
 public class slist {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
+		Node one = new Node(1);
+		one.append(2);
+		one.append(3);
+		one.append(4);
+		one.append(5);
+		one.retvive();
+		one.delete(5);
+		one.append(0);
+		one.retvive();
+	}
 }
